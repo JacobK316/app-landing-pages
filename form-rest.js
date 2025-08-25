e6833dbc4f34c457f010b357d96ade993cb2af01
Your GitHub Username <Your GitHub Email>
2025-08-25 10:30:43 -0400

    added js functionality

--- form-rest.js
+++ form-rest.js
@@ -28,3 +28,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.getElementById("dev-age").textContent = age;
 });
+
+//Count how many times a button is tapped. To use, make a button have an id="myButton"and then have an h2 or something have the id clickCount
+let count = 0;
+
+document.getElementById('myButton').addEventListener('click', () => {
+  count++;
+  document.getElementById('clickCount').textContent = `Clicked ${count} times`;
+  console.log(`Button clicked ${count} times`);
+});
